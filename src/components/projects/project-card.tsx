7cea0c32defbe9d3b7110bca35aa805f94731dc1
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2024-07-28 19:59:19 +0545

    Support dark theme styling

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -12,13 +12,13 @@ export default function ProjectCard({projectImage, title, description, link}: {
     const router = useRouter();
     return (
         <div
-            className={"bg-white border-[#eaeaea] rounded-md flex flex-col border-[1px] p-2 h-[100%]"}>
+            className={"bg-white dark:bg-neutral-dark dark:border-solid dark:border-white dark:border-[1px] border-[#eaeaea] rounded-md flex flex-col border-[1px] p-2 h-[100%]"}>
             <div className={"px-4"}>
-                <div className={"flex gap-2 mb-2 items-center"}>
+                <div className={"flex gap-2 mb-2 text-neutral-dark dark:text-neutral-light items-center"}>
                     <LinkIcon className={"!w-5 !h-5 cursor-pointer"} onClick={() => router.push(link)}/>
                     <h1 className={"font-bold text-lg"}>{title} </h1>
                 </div>
-                <p className={"text-gray-600"}>{description}</p>
+                <p className={"text-gray-600 dark:text-neutral-light"}>{description}</p>
             </div>
         </div>
     )
